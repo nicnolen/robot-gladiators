999894db36e9e3ac6dcfa35074ac472ebde3777d
Nicholas Nolen <nicnolen@ymail.com>
2021-11-15 14:17:13 -0600

    added variables and logged them to the console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!");
+//Create a robots name
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//This will do math and log 20
+console.log(10 + 10);
+//What is this?
+console.log("Our robot's name is " + playerName);
+
+// Create a function named "fight"
+function fight() {
+    window.alert("The fight has begun");
+}
+// Call the fight function
+// fight();
